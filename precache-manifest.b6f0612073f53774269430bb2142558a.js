3179415b8d762fbe3e4edd9bc36738b8a59413b1
royhuang <r06521601@ntu.edu.tw>
2020-04-13 02:02:28 +0800

    Updates

---
+++ precache-manifest.b6f0612073f53774269430bb2142558a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c642d06150230426b6119b89ed1cb48a",
+    "url": "/whichCorona/index.html"
+  },
+  {
+    "revision": "1f3b1c200f94fa1c407c",
+    "url": "/whichCorona/static/css/2.9d81b2d4.chunk.css"
+  },
+  {
+    "revision": "c0e55aa3197fe3ec140c",
+    "url": "/whichCorona/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1f3b1c200f94fa1c407c",
+    "url": "/whichCorona/static/js/2.87ed336c.chunk.js"
+  },
+  {
+    "revision": "0fb0e672f1308cb5b7677cdb93fc42ab",
+    "url": "/whichCorona/static/js/2.87ed336c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0e55aa3197fe3ec140c",
+    "url": "/whichCorona/static/js/main.10fca6ce.chunk.js"
+  },
+  {
+    "revision": "fa8624ea848a38bac4ed",
+    "url": "/whichCorona/static/js/runtime-main.07e32884.js"
+  },
+  {
+    "revision": "11b61240b2b775d92af904f78ac24eee",
+    "url": "/whichCorona/static/media/coffin.11b61240.mp4"
+  },
+  {
+    "revision": "21aaa9debdd4fb80e9846ca7d13cdb4e",
+    "url": "/whichCorona/static/media/corona.21aaa9de.png"
+  },
+  {
+    "revision": "80ce1ac36342a1dca9dce9a6891553d2",
+    "url": "/whichCorona/static/media/coronavirus.80ce1ac3.png"
+  },
+  {
+    "revision": "c0aa3ddd5ba8c488ce32373546587994",
+    "url": "/whichCorona/static/media/stare.c0aa3ddd.png"
+  }
+]);
